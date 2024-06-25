e8d16598da13d668a8385c10fc288a3f8efe3c69
ADMIN <ADMIN@PC223>
2024-06-25 11:28:07 +0700

    Add project files.

---
+++ WebBanHang/Models/EmailSender.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebBanHang.Models
+{
+    public class EmailSender : Controller
+    {
+        public Task SendEmailAsync (string email, string subject, String htmlMessage)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
